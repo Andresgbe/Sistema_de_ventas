992e38f8b100c364738ad23ace5682969a4bf327
Andres <andresgilbe2021@gmail.com>
2024-05-20 00:06:59 -0400

    Creando el repositorio

---
+++ src/login.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./App.css";
+import PropTypes from 'prop-types';
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin(username, password);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+Login.propTypes = {
+  onLogin: PropTypes.func.isRequired,
+};
+
+export default Login;
---
+++ src/productos.js
@@ -0,0 +1,11 @@
+const Andres = ["1","2"];
+
+
+
+export function Frases() {
+    const andres = ["hola","chao"]
+    return [andres[0]]
+}
+
+
+
